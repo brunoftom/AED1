fe8574c87520d6062db269b69b203ffcfa025255
Bruno Ferreira <49572026+brunoftom@users.noreply.github.com>
2022-06-27 20:33:51 -0300

    Create lista.h

---
+++ LAB06/EX.02/lista.h
@@ -0,0 +1,14 @@
+#ifndef LISTA_H_INCLUDED
+#define LISTA_H_INCLUDED
+
+typedef struct no *Lista;
+
+Lista cria_lista();
+int lista_vazia(Lista lst);
+int insere_elem(Lista *lst, int elem);
+int remove_elem(Lista *lst, int elem);
+int obtem_valor_elem(Lista lst, int i, int *nro);
+int get_end(Lista lst);
+void print1(Lista lst);
+
+#endif // LISTA_H_INCLUDED
